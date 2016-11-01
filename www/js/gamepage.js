f947dc7ef6a4c69a8fe9789528c4fe10393ee2fa
riordanalfredo <rdalf2@student.monash.edu>
2016-11-01 19:52:12 +1100

    Game Mechanism
    50% done

--- www/js/gamepage.js
+++ www/js/gamepage.js
@@ -9,18 +9,46 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
-var colors = ["red", "green", "blue", "purple", "yellow", "orange","teal"];
+var colors = ["red", "green", "blue", "yellow"];
+var currentColor = 0;
+var index = [];
 
-    var currentColor = 0;
     function switchColor() {    
-        if (currentColor >= colors.length) currentColor = 0;
-
-        $('#theDiv').css('background-color', colors[currentColor++]);
+       
+        var randomColorIndex =  Math.floor(Math.random() * 4);
+        
+         index.push(randomColorIndex);
+         randomColorIndex = checkerFunction(randomColorIndex);
+        
+        function checkerFunction(random){
+            
+            var value = 0;
+            if(index.length == 2) { 
+                if (index[0] == index[1])
+                     {
+                        value = Math.floor(Math.random() * 4);
+                        index = [random];
+                        index.push(value);
+                        value = checkerFunction(value);
+                        index = []; //clearing
+                     }
+                else {
+                        value = random; //if it is not similar, return the correct value
+                        index = [];
+                     }
+             }
+            else { value = random }
+            return value;
+        }
+         
+        $('#theDiv').css('background-color', colors[randomColorIndex]);
         var now = new Date();
+       
+        
         setTimeout(switchColor, 1000);
     }
 
-    setTimeout(switchColor,5000);  
+    setTimeout(switchColor,0);  
 
         var intervalId = setInterval(updateTime, 0);;
         var progressbar = document.getElementById("progressbar");
@@ -28,7 +56,57 @@ var colors = ["red", "green", "blue", "purple", "yellow", "orange","teal"];
         function updateTime()
         {
             var now = new Date();
-            progressbar.value = now.getMilliseconds();
+            progressbar.value = now.getMilliseconds()*2;
             //progressbar.setAttribute("value", now.getMilliseconds());
         }
 
+//------------------------------------------------------------------------//
+
+var score = 0;
+var answer = document.getElementById('score');
+answer.innerHTML = score;
+
+function scoreCalculator(){
+    score += 1;
+    answer.innerHTML = score;
+}
+
+//---------------------------------Random Answer Choice--------------------------------------//
+var choice = ["",""];
+var colors = ["RED", "GREEN", "BLUE", "YELLOW", "GREEN"];
+
+setTimeout(randomChoices,0);
+setInterval(randomChoices,1000);
+
+var answera = document.getElementById("answera");
+var answerb = document.getElementById("answerb");
+    
+    
+function randomChoices(){
+    
+            var timing = Math.floor(Math.random()*4);
+            var timing1 = Math.floor(Math.random()*4);
+            var random1 = timing1 * Math.floor(timing / 3);
+            var random2 = timing * Math.floor(timing1 / 2);
+    
+            if (random1 == random2){
+                
+                var operatorRandom = Math.floor(Math.random()*3) + 1;
+                
+                if ( random1 <= 3)
+                    {
+                        random2 = Math.abs(random2 - operatorRandom);
+                    }
+                else if ( random1 == 0 )
+                    {
+                        random2 = random2 + operatorRandom;
+                    }
+            }
+            answera.innerHTML = colors[random1];
+            answerb.innerHTML = colors[random2];
+            
+}
+
+
+
+ 
