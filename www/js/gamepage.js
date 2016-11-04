1d22bcd9f68473d14e4b02d9c0cd6b6e074fc211
riordanalfredo <rdalf2@student.monash.edu>
2016-11-04 12:37:15 +1100

    Upgrading Game Mechanism
    Haven't finished with alert when game over

--- www/js/gamepage.js
+++ www/js/gamepage.js
@@ -9,26 +9,29 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
-var colors = ["red", "green", "blue", "yellow"];
+var colors = ["RED", "GREEN", "BLUE", "YELLOW"];
 var currentColor = 0;
 var index = [];
-var timer = 1500;
+var timer = 1000;
+var colorVault = [];
 
-    function switchColor() {    
-       
-        var randomColorIndex =  Math.floor(Math.random() * 4);
+function switchColor() {    
+            
+            var randomColorIndex =  Math.floor(Math.random() * 4);
         
-         index.push(randomColorIndex);
-         randomColorIndex = checkerFunction(randomColorIndex);
-         
-        $('#theDiv').css('background-color', colors[randomColorIndex]);
-        var now = new Date();
-       
-        
-        setTimeout(switchColor, timer);
+            index.push(randomColorIndex);
+            randomColorIndex = checkerFunction(randomColorIndex);
+            var randomColor = colors[randomColorIndex];
+    
+            $('#theDiv').css('background-color', randomColor);
+    
+            colorVault.push(randomColor);
+            
     }
 
-    function checkerFunction(random){
+setTimeout(switchColor,0); //Running Switch Color 
+
+function checkerFunction(random){
             
             var value = 0;
             if(index.length == 2) { 
@@ -48,33 +51,12 @@ var timer = 1500;
             else { value = random }
             return value;
         }
+ 
 
-    setTimeout(switchColor,0);  
-
-        var intervalId = setInterval(updateTime, 0);;
-        var progressbar = document.getElementById("progressbar");
-        
-        function updateTime()
-        {
-            var now = new Date();
-            progressbar.value = now.getMilliseconds()*2;
-            //progressbar.setAttribute("value", now.getMilliseconds());
-        }
-
-//------------------------------------------------------------------------//
-
-var score = 0;
-var answer = document.getElementById('score');
-answer.innerHTML = score;
-
-function scoreCalculator(){
-    score += 1;
-    answer.innerHTML = score;
-}
 
 //---------------------------------Random Answer Choice--------------------------------------//
 var choice = ["",""];
-var colors = ["RED", "GREEN", "BLUE", "YELLOW", "GREEN"];
+var colors = ["RED", "GREEN", "BLUE", "YELLOW"];
 
 setTimeout(randomChoices,0);
 setInterval(randomChoices,timer);
@@ -87,7 +69,7 @@ function randomChoices(){
     
             var timing = Math.floor(Math.random()*4);
             var timing1 = Math.floor(Math.random()*4);
-            var random1 = timing1 * Math.floor(timing / 3);
+            var random1 = timing1 * Math.ceil(timing / 3);
             var random2 = timing * Math.floor(timing1 / 2);
     
             if (random1 == random2){
@@ -104,11 +86,106 @@ function randomChoices(){
                         random2 = random2 + operatorRandom;
                     }
             }
+                   
             answera.innerHTML = colors[random1];
-            answerb.innerHTML = colors[random2];
-            
+            answerb.innerHTML = colors[random2];            
+}
+
+// -------------------------------------------------------------------------------------------------------------------------//
+// -------------------------------------------------- Timer CountDown ------------------------------------------------------//
+
+var timeElement = document.getElementById('time');
+timeElement.innerHTML = '7';
+setTimeout(setInterval(timerCountdown,1000),1000);
+ 
+function timerCountdown(){
+    
+    var currentValue = parseInt(timeElement.innerHTML);
+    if (currentValue == 0){
+        
+        var total = currentValue + 0;
+        
+    }
+    else if (currentValue <= -1){
+        
+        var total = currentValue + 1;
+    }
+    
+    else{
+        
+        var total = currentValue - 1;
+    }
+    
+      if (timeElement.innerHTML == "0"){
+        alert("GAME OVER");
+        timeElement.innerHTML = "GAME OVER :("  ;
+      }
+    else{
+         timeElement.innerHTML = total;
+    }
 }
 
 
 
- 
+//-------------------------------------------------- CORRECT OR NOT! -----------------------------------------------------//
+
+var score = 0;
+var answer = document.getElementById('score');
+answer.innerHTML = score;
+
+var storage = [];
+
+function scoreCalculator(i){
+    
+    
+    storage.push(colorVault[colorVault.length - 1]);
+    
+    if ( i == "a")
+        {
+            storage.push(answera.innerHTML);
+        }
+    else if (i == "b"){
+            storage.push(answerb.innerHTML);
+        }
+    
+    if (storage[0] !== storage[1])
+    {
+        wrongAnswer();
+    }
+        else if (storage[0] == storage[1] )
+    {
+        correctAnswer();
+    }
+
+    switchColor();
+}
+
+
+function correctAnswer(){
+    
+    var currentValue = parseInt(timeElement.innerHTML);
+    var total = currentValue + 1;
+
+    timeElement.innerHTML = total;
+    
+    score += 1;
+    answer.innerHTML = score;
+    
+}
+
+function wrongAnswer(){
+    if (timeElement.innerHTML == "0"){
+        alert("GAME OVER");
+    }
+    
+    var currentValue = parseInt(timeElement.innerHTML);
+    var total = currentValue - 1;
+    
+    
+    timeElement.innerHTML = total;
+    
+}
+
+
+
+    
